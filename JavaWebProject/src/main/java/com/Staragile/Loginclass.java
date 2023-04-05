6df891bd1924e2680a98662d013c62ee24f7178d
Harshi <harshithaanand25@gmail.com>
2023-04-05 23:23:36 +0530

    Adding new commit

---
+++ JavaWebProject/src/main/java/com/Staragile/Loginclass.java
@@ -0,0 +1,17 @@
+package com.Staragile;
+
+public class Loginclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Its Dynamic project");
+	
+	int num1= 20;
+	int num2= 30;
+	int sum=a+b;
+		System.out.println("Its Dynamic project");
+		
+		System.out.println("Sum is:" +sum);
+	}
+
+}
